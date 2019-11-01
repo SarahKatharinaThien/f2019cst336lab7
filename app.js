34dc0fc727a3d5a4e50ea02a8d1578affc72080f
Sarah Katharina Thien <sarah.katharina.thien@uni-oldenburg.de>
2019-10-31 21:31:53 -0700

    added logs to app.js

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ const request = require("request");
 // routes
 app.get("/", function (req, res) {
     request('https://pixabay.com/api/?key=13891908-73259bb3df0ab4191e8002f0c', function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
+        //console.log('error:', error); // Print the error if one occurred
+        //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        //console.log('body:', body); // Print the HTML for the Google homepage.
+       console.log(response);
     });
+    console.log("it works!");
 });
 
 // starting server
